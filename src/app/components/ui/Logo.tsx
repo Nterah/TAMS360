c8da845ab881499fb846d01edd5871a08df464be
Nterah <159769474+Nterah@users.noreply.github.com>
2026-01-11 20:19:35 +0200

    Update the logo.tsx

--- src/app/components/ui/Logo.tsx
+++ src/app/components/ui/Logo.tsx
@@ -5,11 +5,9 @@ interface LogoProps {
 }
 
 export default function Logo({ className = "", width = 120, height = 120 }: LogoProps) {
-  // Use the PNG logo files from public folder
-  // These will be replaced with your actual logo when you deploy
   return (
     <img 
-      src="/icon-192x192.png" 
+      src="/favicon.svg" 
       alt="TAMS360 Logo" 
       width={width} 
       height={height} 
